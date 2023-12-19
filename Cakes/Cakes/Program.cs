13a4431b21b6b0429e222622ffa18c78fce6373d
Vladislav <75453047+vladrichard96@users.noreply.github.com>
2023-12-19 13:58:52 +0300

    Add files via upload

---
+++ Cakes/Cakes/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Введите количество гостей на чайном перерыве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+List<int> factors = Factorize(n);
+for (int i = 0; i < factors.Count; i++)
+{
+    sum += PrimeNumber(factors[i]);
+}
+Console.WriteLine(sum);
+static List<int> Factorize (int x)
+{
+    List<int> factors = new List<int>();
+    for (int i = 2; i <= Math.Sqrt(x); i++)
+    {
+        while (x%i == 0)
+        {
+            factors.Add(i);
+            x /= i;
+        }
+    }
+    if (x!=1) factors.Add(x);
+    return factors;
+}
+static int PrimeNumber (int x)
+{
+    int count = 0;
+    for (int i = 2; i <=x; i++)
+    {
+        if (IsPrime(i))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+static bool IsPrime(int number)
+{
+    for (int i = 2; i <= Math.Sqrt(number); i++)
+    {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
+}
